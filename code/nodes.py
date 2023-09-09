c4ee68e9b3df653e031b1ea14dc4950f6aaab022
R.Costa <144382847+RCostaBooks@users.noreply.github.com>
2023-09-09 20:07:51 -0300

    Update nodes.py

--- code/nodes.py
+++ code/nodes.py
@@ -1,40 +1,20 @@
+import pandas as pd
 import logging
 from langchain.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceInstructEmbeddings
-from langchain.vectorstores import Chroma, SKLearnVectorStore
+log = logging.getLogger(__name__)
 
-DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
+def loadDocuments() -> PyPDFDirectoryLoader:
+  log.info("Starting PDFLoader Node.")
 
+  loader = PyPDFDirectoryLoader("./data/01_raw")
+  docs = loader.load()
+  log.info(f"Total pages loaded: {len(docs)}")
+  return docs
 
-def ingestTextFolder() -> ParquetDataSet:
-    log = logging.getLogger(__name__)
-    log.info('Starting to IngestPDFs from Raw Folder...')
-    loader = PyPDFDirectoryLoader("pdfs")
-    docs = loader.load()
-    log.info(f"Total pages loaded: {len(docs)}")
-
-    log.info('Spliting Text into Chunks...')
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=64)
-    texts = text_splitter.split_documents(docs)
-    log.info(f"Total Chunks created: {len(texts)}")
-        
-    log.info('Loading Llama Embedding Model...')
-
-    embeddings = HuggingFaceInstructEmbeddings(
-      model_name="hkunlp/instructor-large",
-      model_kwargs={"device": DEVICE}
-    )
-    
-    log.info('Saving Persistent Vector Database')
-    db = SKLearnVectorStore.from_documents(
-      documents=texts,
-      embeddings=embeddings,
-      persist_path="./data/05_model_input/vectordb.parquet",
-      serializer="parquet"
-      )
-    db.persist()
-
-    #db = Chroma.from_documents(texts, embeddings, persist_directory="./data/#05_model_input")       
-    log.info('Ingestion process completed for all pdf files.')
-    return
+def textSplitting(docs: PyPDFDirectoryLoader) -> RecursiveCharacterTextSplitter:
+  log.info('Splitting Text into Chunks...')
+  text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=64)
+  texts = text_splitter.split_documents(docs)
+  log.info(f"Total Chunks created: {len(texts)}")
+  return texts
